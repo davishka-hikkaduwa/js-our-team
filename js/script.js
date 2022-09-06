7854dbcd5aa6b8ecc25bcf9abe7e8a0fd5befa51
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-09-06 16:01:52 +0200

    milestone-1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,73 @@
 console.log('JS OK!');
 
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+Consigli del giorno:
+Ragioniamo come sempre a step.
+Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+*/
+
+// DATI
+/*
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+*/
+
+
+// ----- MILESTONE 1 -----
+
+const team = [
+    {
+        nome: 'Wayne',
+        cognome: 'Barnett',
+        ruolo: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Caroll',
+        ruolo: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter',
+        cognome: 'Gordon',
+        ruolo: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela',
+        cognome: 'Lopez',
+        ruolo: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott',
+        cognome: 'Estrada',
+        ruolo: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara',
+        cognome: 'Ramos',
+        ruolo: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
